7cb1f109c90f1ddaee8dc457206487fd8e23c4b4
W1W1-M <w1w1_m@icloud.com>
2022-06-18 13:51:00 +0200

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by William Mead on 18/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
